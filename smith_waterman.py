d4a485203e6e96e3506eb769cac503f0efe374ec
adailtonasp <adailton-hag@hotmail.com>
2022-07-29 10:10:35 -0300

    Modificação e adaptação do algoritmo

--- smith_waterman.py
+++ smith_waterman.py
@@ -4,82 +4,99 @@
 # entrada
 s1 = "TGGT"
 s2 = "CGT"
-gap = 0
-match = 0
-mismatch = 0
+gap = -3
+match = 2
+mismatch = -1
 
 # process in
-s1 = s1 + "U"
-s2 = s2 + "U"
+s1 = "U" + s1
+
+s2 = "U" + s2
 
 
 class node:
-    c_from = []
-
-    def __init__(self, value):
+    def __init__(self, value, n1, n2):
         self.value = value
+        self.n1 = n1
+        self.n2 = n2
+        self.op = ""
 
 
-matrix = [list([node(0) for i in range(len(s1))]) for j in range(len(s2))]
+matrix = [list([node(0, s1[i], s2[j]) for i in range(len(s1))])
+          for j in range(len(s2))]
 
-for i in range(len(s2)):
-    matrix[i][0].value = gap*i
+for j in range(len(s2)):
+    matrix[j][0].value = gap * j
 
-matrix[0] = [node(gap*i) for i in range(len(s1))]
+for i in range(len(s1)):
+    matrix[0][i].value = gap * i
+
+# for j in range(len(s2)):
+#     for i in range(len(s1)):
+#         print(matrix[j][i].value, end=' ')
+#     print("\n")
+
+# print("========================================")
 
 
-def set_value(c_node, left, diagonal, bottom, gap, match, mismatch, i, j, n1, n2):
-
-    left = left.value+gap
-
+def set_value(matrix, act_node, gap, mismatch, match):
+    n1 = [act_node[0]][act_node[1]]
+    n2 = matrix[act_node[0]][act_node[1]]
     if n1 == n2:
-        diagonal = diagonal.value+match
+        match = matrix[act_node[0]-1][act_node[1]-1].value + match
     else:
-        diagonal = diagonal.value+mismatch
+        mismatch = matrix[act_node[0]-1][act_node[1]-1].value + mismatch
 
-    bottom = bottom.value + gap
+    gap_left = matrix[act_node[0]][act_node[1]-1].value + gap
 
-    value_list = [left, diagonal, bottom]
+    gap_top = matrix[act_node[0]-1][act_node[1]].value + gap
 
-    value_list.sort()
+    dict_value = {"match": match, "mismatch": mismatch,
+                  "gap_left": gap_left, "gap_top": gap_top}
 
-    value = value_list[-1]
+    # print(dict_value)
 
-    c_node.value = value
+    matrix[act_node[0]][act_node[1]].op = max(dict_value)
 
-    # link node
-    if value == left:
-        c_node.c_from.append((i, j+1))
-    if value == diagonal:
-        c_node.c_from.append((i+1, j+1))
-    if value == bottom:
-        c_node.c_from.append((i+1, j))
+    matrix[act_node[0]][act_node[1]].value = dict_value[max(dict_value)]
 
 
-for i in range(len(s2)-1):
-    for j in range(len(s1)-1):
-        set_value(matrix[i+1][j+1], matrix[i][j+1], matrix[i][j],
-                  matrix[i+1][j], gap, match, mismatch, i, j, s1[j], s2[i])
+for j in range(len(s2)-1):
+    for i in range(len(s1)-1):
+        set_value(matrix, (j+1, i+1), gap, mismatch, match)
 
-# process out
-seq_alinhada_s1 = ""
-seq_alinhada_s2 = ""
+for j in range(len(s2)):
+    for i in range(len(s1)):
+        print(matrix[j][i].value, end=' ')
+    print("\n")
 
-# do canto superior direito
-i = len(s2)-1
-j = len(s1)-1
-act_node = matrix[i][j]
+# BACKTRACE
+act_node = [len(s2)-1, len(s1)-1]
 
-while(len(act_node.c_from) != 0):
-    seq_alinhada_s1 = seq_alinhada_s1+s1[i]
-    seq_alinhada_s2 = seq_alinhada_s2+s2[j]
+seq_s1 = ""
+seq_s2 = ""
 
-    i = act_node.c_from[0][0]
-    j = act_node.c_from[0][1]
+seq_s1 = seq_s1 + matrix[act_node[0]][act_node[1]].n1
+seq_s2 = seq_s2 + matrix[act_node[0]][act_node[1]].n2
 
-    act_node = matrix[i][j]
-
-# correcao
-if i != 0 and j != 0:
-
-    exit
+while(True):
+    if matrix[act_node[0]][act_node[1]].op == "match":
+        act_node[0] = act_node[0] - 1
+        act_node[1] = act_node[1] - 1
+        seq_s1 = seq_s1 + matrix[act_node[0]][act_node[1]].n1
+        seq_s2 = seq_s2 + matrix[act_node[0]][act_node[1]].n2
+    elif matrix[act_node[0]][act_node[1]].op == "mismatch":
+        act_node[0] = act_node[0] - 1
+        act_node[1] = act_node[1] - 1
+        seq_s1 = seq_s1 + matrix[act_node[0]][act_node[1]].n1
+        seq_s2 = seq_s2 + matrix[act_node[0]][act_node[1]].n2
+    elif matrix[act_node[0]][act_node[1]].op == "gap_left":
+        act_node[0] = act_node[0] - 1
+        seq_s1 = seq_s1 + "-"
+        seq_s2 = seq_s2 + matrix[act_node[0]][act_node[1]].n2
+    elif matrix[act_node[0]][act_node[1]].op == "gap_top":
+        act_node[1] = act_node[1] - 1
+        seq_s1 = seq_s1 + matrix[act_node[0]][act_node[1]].n1
+        seq_s2 = seq_s2 + "-"
+    else:
+        break
