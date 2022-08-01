6839dcd206776456bc4263b9cbdc857ae7e4fef7
adailtonasp <adailton-hag@hotmail.com>
2022-08-01 16:10:05 -0300

    Adição da correção ao nó quando chega a um ponto de partida

--- smith_waterman.py
+++ smith_waterman.py
@@ -2,10 +2,12 @@
 # smith waterman
 
 # entrada
-s2 = "CCCCCCCCGCUCCACUCUGU"
-s1 = "CCCCUCCCCUCCC"
+
+s2 = "GUGGGCUGGAAGCAGCGACGGCUGGAAGGAUUGAACCACACCGACGCGAGAAGCCUACCUGAACUGGUAUUGGUUGAUUCGUUUAUGCCAACAUCGCGGCUCUUCUCUUAUCCGCCGAUUAUCGCAGAAGCCGCUCGGGCUGUUUCUCUUCUGCCCGGGUGAAGUCUCAGGGGUGCACACCACAUCUGAUCUCUUUACAA"
+s1 = "GUGGAAGCAGCGACGUGACGGCUGGAAGGAUUGAACCGACGCGAGAAGCCUACCUGAACUGGUAUUGGUUGACUUGAUUCGUUUAUGCCAACAUCGCGGCUCUUAUUCUAACCUAUCCGCCGAUUAUCGCAGA"
+
 gap = -3
-match = 2
+match = 1
 mismatch = -1
 
 # process in
@@ -109,5 +111,34 @@ while(True):
     else:
         break
 
-print(seq_s1+"\n")
-print(seq_s2+"\n")
+#CORRECAO
+if act_node[0] == 0 : #siginica que esta na primeira coluna
+    for i in range(act_node[1]):
+        seq_s2 = seq_s2 + "-"
+        seq_s1 = seq_s1 + matrix[act_node[0]][i].n1
+elif act_node[1] == 0:
+    for j in range(act_node[0]):
+        seq_s1 = seq_s1 + "-"
+        seq_s2 = seq_s2 + matrix[j][act_node[j]].n2
+
+print(seq_s1[::-1]+"\n")
+print(seq_s2[::-1]+"\n")
+
+act_node = [len(s2)-1, len(s1)-1]
+
+meta_value = matrix[act_node[0]][act_node[1]].value
+
+sum_value = 0
+for i in range(len(seq_s1)):
+    if seq_s1[i] == "-" and seq_s2[i] == "-":
+        sum_value = sum_value
+        print("Double Gap")
+    elif seq_s1[i] == "-" or seq_s2[i] == "-":
+        sum_value = sum_value  + gap
+    elif seq_s1[i] != seq_s2[i]:
+        sum_value = sum_value + mismatch
+    else:
+        sum_value = sum_value + match
+
+print("meta_value",meta_value)
+print("sum_value",sum_value)
