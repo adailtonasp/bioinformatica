99bebd1b849b35f3cc776c182686935e221e255f
adailtonasp <adailton-hag@hotmail.com>
2022-09-12 11:23:29 -0300

    adicao do algoritmo de grafo de bruijn

---
+++ grafo-bruijn.py
@@ -0,0 +1,21 @@
+# Implementação do pareamento de bruijn
+
+# Enumere cada entrada k-mer(d,m)
+# d é o tamanho da virtualizaçao
+# m é o tamanho da string a ser processado
+# n é  o tamanho da entrada
+
+# Produza cada nó separado onde cada nó é dividido em elemento a e elemento b
+
+# Para cada elemento a/b de cada nó verifique se existe alguem nó com elemento b/a igual
+
+# Se existir vincule ese outro nó ao nó atual e prossiga a verificação
+
+# Identifique a raíz da arvore montada - no com elemento a vazio/nulo
+
+# Monte a matriz de pareamento com tamanho igual a n x (2 * m + d) + n - 1 << verificar
+# inicialize com valor nulo representativo
+
+# preencha seguindo o algoritimo convencional
+
+# fim
