0e1311ab961cdd4d50a639e9e27d1e4d76bcd4ec
adailtonasp <adailton-hag@hotmail.com>
2022-09-22 14:35:35 -0300

    adicao da criacao de nos e arcos do algoritmo

--- grafo-bruijn.py
+++ grafo-bruijn.py
@@ -19,3 +19,51 @@
 # preencha seguindo o algoritimo convencional
 
 # fim
+
+k = 3
+
+lista_entrada=("ATT","GCC","CAT","TTG","CCA","TGC","ATC","TCC","TGA","GAC","CAT","ATG","CCA")
+
+class node:
+    def __init__(self,value):    
+        self.value = value
+        self.grau_entrada = 0
+        self.grau_saida = 0
+
+class arco:
+    def __init__(self,node_saida,node_entrada,rotulo):
+        self.node_saida = node_saida
+        self.node_entrada = node_entrada
+        self.rotulo = rotulo
+
+lista_node = []
+lista_arco = []
+
+for i in lista_entrada:
+    prefixo = i[:k-1]
+    sufixo = i[1:k]
+    
+    node_p = None
+    node_s = None
+
+    if prefixo not in [lista_node[i].value for i in range(len(lista_node))]:
+        node_p = node(prefixo)
+        lista_node.append(node_p)
+    else:
+        node_p = [lista_node[x] for x in range(len(lista_node)) if lista_node[x].value == prefixo][0]
+
+    if sufixo not in [lista_node[i].value for i in range(len(lista_node))]:
+        node_s = node(sufixo)
+        lista_node.append(node_s)
+    else:
+        node_s = [lista_node[x] for x in range(len(lista_node)) if lista_node[x].value == sufixo][0]
+
+    arco_c = arco(node_p,node_s,i)
+
+    arco_c.node_saida.grau_saida += 1
+
+    arco_c.node_entrada.grau_entrada += 1
+
+    lista_arco.append(arco_c)
+
+input()
