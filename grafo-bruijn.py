b6236587f9d0e551cebd01cf966ab75fd9f7786f
adailtonasp <adailton-hag@hotmail.com>
2022-09-26 09:20:43 -0300

    implementacao do algoritmo de bruijn

--- grafo-bruijn.py
+++ grafo-bruijn.py
@@ -1,34 +1,15 @@
-# Implementação do pareamento de bruijn
-
-# Enumere cada entrada k-mer(d,m)
-# d é o tamanho da virtualizaçao
-# m é o tamanho da string a ser processado
-# n é  o tamanho da entrada
-
-# Produza cada nó separado onde cada nó é dividido em elemento a e elemento b
-
-# Para cada elemento a/b de cada nó verifique se existe alguem nó com elemento b/a igual
-
-# Se existir vincule ese outro nó ao nó atual e prossiga a verificação
-
-# Identifique a raíz da arvore montada - no com elemento a vazio/nulo
-
-# Monte a matriz de pareamento com tamanho igual a n x (2 * m + d) + n - 1 << verificar
-# inicialize com valor nulo representativo
-
-# preencha seguindo o algoritimo convencional
-
-# fim
 
 k = 3
 
 lista_entrada=("ATT","GCC","CAT","TTG","CCA","TGC","ATC","TCC","TGA","GAC","CAT","ATG","CCA")
 
+string_saida= ""
+
 class node:
     def __init__(self,value):    
         self.value = value
-        self.grau_entrada = 0
-        self.grau_saida = 0
+        self.lista_entrada = []
+        self.lista_saida = []
 
 class arco:
     def __init__(self,node_saida,node_entrada,rotulo):
@@ -60,10 +41,59 @@ for i in lista_entrada:
 
     arco_c = arco(node_p,node_s,i)
 
-    arco_c.node_saida.grau_saida += 1
+    arco_c.node_saida.lista_saida.append(arco_c)
 
-    arco_c.node_entrada.grau_entrada += 1
+    arco_c.node_entrada.lista_entrada.append(arco_c)
 
     lista_arco.append(arco_c)
+
+init_node = None
+final_node = None
+
+for i in lista_node:
+    if len(i.lista_entrada) - len(i.lista_saida) < 0: 
+        init_node = i
+
+    if len(i.lista_entrada) - len(i.lista_saida) > 0:
+        final_node = i
+
+    if init_node != None and final_node != None:
+        break
+
+node_atual = init_node
+
+#correcao para inicio do caminho no ciclo
+while(len(node_atual.lista_saida) == 1 and len(lista_arco) > 0):
+    string_saida = string_saida + node_atual.value
+
+    lista_arco.remove(node_atual.lista_saida[0])
+
+    node_atual = node_atual.lista_saida[0].node_entrada  
+
+#correcao para finalizacao do caminho no ciclo
+string_saida_anexo = ""
+while(len(final_node.lista_entrada) == 1 and len(lista_arco) > 0):
+    string_saida_anexo = final_node.value + string_saida_anexo
+
+    lista_arco.remove(final_node.lista_entrada[0])
+
+    final_node = final_node.lista_entrada[0].node_saida
+
+arco_atual = node_atual.lista_saida[0]
+
+ciclo = []
+
+while (len(lista_arco) != 0):
+
+    while(node_atual.value != arco_atual.node_entrada.value):
+        ciclo.append(arco_atual)
+        node_atual = arco_atual.node_entrada
+        arco_atual = node_atual.lista_saida[0]
+        node_atual.lista_saida.remove(arco_atual)
+
+    for i in ciclo:
+        lista_arco.remove(i)
+    
+    ciclo = []
 
 input()
