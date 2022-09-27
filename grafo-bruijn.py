900ca10c8c365d89a8b54f7d30ba08a262207390
adailtonasp <adailton-hag@hotmail.com>
2022-09-27 17:57:51 -0300

    incremento do codigo

--- grafo-bruijn.py
+++ grafo-bruijn.py
@@ -1,33 +1,24 @@
 k = 3
 
-lista_entrada=("ATT","GCC","CAT","TTG","CCA","TGC","ATC","TCC","TGA","GAC","CAT","ATG","CCA")
+#lista_entrada=("ATT","GCC","CAT","TTG","CCA","TGC","ATC","TCC","TGA","GAC","CAT","ATG","CCA")
 
+with open("entrada_bruijn.txt", 'r') as file_w:
+    lista_entrada = file_w.readline().split(",")
+        
 
-def setArcoNodeSaida(arco,node):
-    node.lista_saida.append(arco)
-
-def setArcoNodeEntrada(arco,node):
-    node.lista_entrada.append(arco)
-
-class node():
-    lista_entrada = list([object])
-    lista_saida = list([object])
+class node:
+    grau_entrada = 0
+    grau_saida = 0
     def __init__(self,value):
         self.value = value
 
-class arco():
-
-    def bindingNodeArco(self):
-        self.node_saida.lista_saida.append(self)
-        self.node_entrada.lista_saida.pop()
-        self.node_entrada.lista_entrada.append(self)
-        self.node_saida.lista_entrada.pop()
-
-    def __init__(self,ns,ne,rotulo):
-        self.node_saida = ns
-        self.node_entrada = ne
+class arco:
+    def __init__(self,node_saida,node_entrada,rotulo):
+        node_saida.grau_saida += 1
+        node_entrada.grau_entrada += 1
+        self.node_saida = node_saida
+        self.node_entrada = node_entrada
         self.rotulo = rotulo
-        self.bindingNodeArco()
 
 lista_node = []
 lista_arco = []
@@ -58,16 +49,17 @@ for i in lista_entrada:
 init_node = None
 final_node = None
 
-for i in lista_node:
-    if len(i.lista_entrada) - len(i.lista_saida) < 0: 
-        init_node = i
-
-    if len(i.lista_entrada) - len(i.lista_saida) > 0:
-        final_node = i
-
-    if init_node != None and final_node != None:
+#identificar nos de inicio e fim
+i = 0
+while(True):
+    if lista_node[i].grau_entrada > lista_node[i].grau_saida:
+        final_node = lista_node[i]
+    if lista_node[i].grau_entrada < lista_node[i].grau_saida:
+        init_node = lista_node[i]
+    i += 1
+    if(init_node != None and final_node != None):
         break
-
+    
 node_atual = init_node
 
 string_saida= ""
@@ -77,33 +69,44 @@ try:
 except:
     print("Nao foi possivel definir um no inicial")
     exit()
+
 #correcao para inicio do caminho no ciclo
-while(len(node_atual.lista_saida) == 1 and len(lista_arco) > 0):
+while node_atual.grau_saida == 1 and node_atual.grau_entrada == 0:
 
-    arco_c = lista_arco.pop(lista_arco.index(node_atual.lista_saida[0])) #pode ser mais simples
+    #encontra um arco cuja saida e o no atual
+    arco_c = [lista_arco[x] for x in range(len(lista_arco)) if lista_arco[x].node_saida == node_atual][0]
 
-    node_atual.lista_saida.remove(arco_c)
+    #remova esse arco da lista de arcos e atualiza o grau
 
-    node_atual = node_atual.lista_saida[0].node_entrada
+    lista_arco.remove(arco_c)
 
-    node_atual.lista_entrada.remove(arco_c)
+    #nao e necessario atualizar o grau de cada no ?
+
+    node_atual.grau_saida -= 1
+
+    node_atual = arco_c.node_entrada
+
+    node_atual.grau_entrada -= 1
 
     string_saida = string_saida + node_atual.value[-1]
+
 #correcao para finalizacao do caminho no ciclo
 string_saida_anexo = ""
-while(len(final_node.lista_entrada) == 1 and len(lista_arco) > 0):
+while final_node.grau_entrada == 1 and final_node.grau_saida == 0:
     
     string_saida_anexo = final_node.value[-1] + string_saida_anexo
 
-    arco_c = lista_arco.pop(lista_arco.index(final_node.lista_entrada[0])) #pode ser mais simples
+    arco_c = [lista_arco[x] for x in range(len(lista_arco)) if lista_arco[x].node_entrada == final_node][0]
 
-    final_node.lista_entrada.remove(arco_c)
+    lista_arco.remove(arco_c)
 
-    final_node = final_node.lista_entrada[0].node_saida
+    final_node.grau_entrada -= 1
 
-    final_node.lista_saida.remove(arco_c)
+    final_node = arco_c.node_saida
 
-arco_c = node_atual.lista_saida[0] #o primeiro dos arcos que estao saindo do no
+    final_node.grau_saida -= 1
+
+arco_c = [lista_arco[x] for x in range(len(lista_arco)) if lista_arco[x].node_saida == node_atual][0]
 
 ciclo_node_v = [] #ciclo de valores de no
 
@@ -112,16 +115,26 @@ while (len(lista_arco) != 0):
     while True:
         ciclo_node_v.append(node_atual.value)
         #atualiza as referencias dos nos que envolver o arco atual
-        node_atual.lista_saida.remove(arco_c)
+
+        node_atual.grau_saida -= 1
+
         node_atual = arco_c.node_entrada
-        node_atual.lista_entrada.remove(arco_c)
-        arco_c = node_atual.lista_saida[0]
+
+        node_atual.grau_entrada -= 1
+
         string_saida = string_saida + node_atual.value[-1]
+
+        lista_arco.remove(arco_c)
+
+        if(len(lista_arco) == 0):
+            break
+
+        arco_c = [lista_arco[x] for x in range(len(lista_arco)) if lista_arco[x].node_saida == node_atual][0]
+
         if node_atual.value in ciclo_node_v:
             break
-    
+            
     ciclo_node_v = []
 
 string_saida = string_saida + string_saida_anexo
-
 input(string_saida)
